f9397581aee586276fcca3dfb9c64b29f94f27d4
wolwerr <ricardo@dtmm.com.br>
2023-12-04 23:00:20 -0300

    "Enviando microserviço Pagamento"

---
+++ src/main/java/com/totem/pagamento/domain/Pagamento.java
@@ -0,0 +1,25 @@
+package com.totem.pagamento.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pagamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String codigo;
+    private String status;
+    private Long pedidoId;
+
+}
---
+++ src/main/java/com/totem/pagamento/infrastructure/repository/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.totem.pagamento.infrastructure.repository;
+
+import com.totem.pagamento.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PagamentoRepository extends JpaRepository<Pagamento, Long>{
+
+    Optional<Pagamento> findByCodigo(String codigo);
+}
